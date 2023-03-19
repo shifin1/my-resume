b0d3e162c68c8629e3e072b0b3867f0ad00e63da
Shifin <mohd.shifinshad@gmail.com>
2023-03-19 13:01:03 +0530

    add download pdf btn and large screen design

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// const downloadBtn = document.getElementById("dwnld-btn");
 
+// downloadBtn.addEventListener("click", () => {
+//   const element = this.document.getElementById("pdf");
+//   var opt = {
+//     margin: 1,
+//     filename: "shifin.pdf",
+//     image: { type: "jpeg", quality: 0.98 },
+//     html2canvas: { scale: 2 },
+//     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+//   };
+//   html2pdf(element, opt);
+// });
